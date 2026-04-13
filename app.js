a9d0767bfeeb3d99cff03dabc033cda6b38f9649
Iscotech <isiaqabiola2019@gmail.com>
2026-04-13 21:15:13 +0100

    new commit after deploy to server

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// ===============================
+
 // SELECT ELEMENTS
-// ===============================
+
 const checkbox = document.querySelector('[data-testid="test-todo-complete-toggle"]');
 const title = document.querySelector('[data-testid="test-todo-title"]');
-const status = document.querySelector('[data-testid="test-todo-status"]');
+const statusUpdate = document.querySelector('[data-testid="test-todo-status"]');
 const timeRemainingEl = document.querySelector('[data-testid="test-todo-time-remaining"]');
 const dueDateEl = document.querySelector('[data-testid="test-todo-due-date"]');
-
 const editBtn = document.querySelector('[data-testid="test-todo-edit-button"]');
 const deleteBtn = document.querySelector('[data-testid="test-todo-delete-button"]');
 
-// ===============================
+
 // SET DUE DATE
-// ===============================
+
 const dueDate = new Date(dueDateEl.getAttribute("datetime"));
 
-// ===============================
+
 // FUNCTION: CALCULATE TIME REMAINING
-// ===============================
+
 function getTimeRemaining() {
   const now = new Date();
   const diff = dueDate - now;
@@ -40,9 +39,9 @@ function getTimeRemaining() {
   return "Due now!";
 }
 
-// ===============================
+
 // UPDATE TIME REMAINING
-// ===============================
+
 function updateTime() {
   timeRemainingEl.textContent = getTimeRemaining();
 }
@@ -53,36 +52,35 @@ updateTime();
 // Update every 60 seconds
 setInterval(updateTime, 60000);
 
-// ===============================
+
 // CHECKBOX TOGGLE
-// ===============================
 checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
     title.style.textDecoration = "line-through";
     title.style.opacity = "0.6";
 
-    status.textContent = "Done";
-    status.style.color = "green";
+    statusUpdate.textContent = "Done";
+    statusUpdate.style.color = "green";
   } else {
     title.style.textDecoration = "none";
     title.style.opacity = "1";
 
-    status.textContent = "Pending";
-    status.style.color = "orange";
+    statusUpdate.textContent = ` Pending`;
+    statusUpdate.style.color = "orange";
   }
 });
 
-// ===============================
+
 // EDIT BUTTON
-// ===============================
+
 editBtn.addEventListener("click", () => {
   console.log("Edit clicked");
   alert("Edit task clicked");
 });
 
-// ===============================
+
 // DELETE BUTTON
-// ===============================
+
 deleteBtn.addEventListener("click", () => {
   console.log("Delete clicked");
 
